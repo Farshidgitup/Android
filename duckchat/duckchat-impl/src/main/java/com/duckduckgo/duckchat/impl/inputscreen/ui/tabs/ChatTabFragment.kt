d17b4892219f42708d56b2dbaab8dd275bc8f18f
Youssef Keyrouz <ykeyrouz@Youssefs-MacBook-Pro.local>
2026-02-04 22:01:20 -0800

    Add UI components for the recent AI chats list

--- duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/inputscreen/ui/tabs/ChatTabFragment.kt
+++ duckchat/duckchat-impl/src/main/java/com/duckduckgo/duckchat/impl/inputscreen/ui/tabs/ChatTabFragment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 DuckDuckGo
+ * Copyright (c) 2026 DuckDuckGo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,10 +16,65 @@
 
 package com.duckduckgo.duckchat.impl.inputscreen.ui.tabs
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.duckduckgo.anvil.annotations.InjectWith
 import com.duckduckgo.common.ui.DuckDuckGoFragment
+import com.duckduckgo.common.ui.viewbinding.viewBinding
+import com.duckduckgo.common.utils.FragmentViewModelFactory
 import com.duckduckgo.di.scopes.FragmentScope
 import com.duckduckgo.duckchat.impl.R
+import com.duckduckgo.duckchat.impl.databinding.FragmentChatTabBinding
+import com.duckduckgo.duckchat.impl.inputscreen.ui.suggestions.ChatSuggestionsAdapter
+import com.duckduckgo.duckchat.impl.inputscreen.ui.viewmodel.InputScreenViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import logcat.logcat
+import javax.inject.Inject
 
 @InjectWith(FragmentScope::class)
-class ChatTabFragment : DuckDuckGoFragment(R.layout.fragment_chat_tab)
+class ChatTabFragment : DuckDuckGoFragment(R.layout.fragment_chat_tab) {
+
+    @Inject
+    lateinit var viewModelFactory: FragmentViewModelFactory
+
+    private val viewModel: InputScreenViewModel by lazy {
+        ViewModelProvider(requireParentFragment(), viewModelFactory)[InputScreenViewModel::class.java]
+    }
+
+    private val binding: FragmentChatTabBinding by viewBinding()
+    private lateinit var chatSuggestionsAdapter: ChatSuggestionsAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupChatSuggestions()
+        observeChatSuggestions()
+    }
+
+    private fun setupChatSuggestions() {
+        chatSuggestionsAdapter = ChatSuggestionsAdapter { suggestion ->
+            // TODO: Handle navigation to chat in duck.ai fullscreen mode
+            logcat { "Chat suggestion clicked: chatId=${suggestion.chatId}, title=${suggestion.title}" }
+        }
+
+        binding.chatSuggestionsRecyclerView.adapter = chatSuggestionsAdapter
+    }
+
+    private fun observeChatSuggestions() {
+        viewModel.chatSuggestions
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
+            .onEach { suggestions ->
+                val hasSuggestions = suggestions.isNotEmpty()
+                binding.chatSuggestionsRecyclerView.isVisible = hasSuggestions
+                binding.recentChatsHeader.isVisible = hasSuggestions
+                chatSuggestionsAdapter.submitList(suggestions)
+            }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+}
